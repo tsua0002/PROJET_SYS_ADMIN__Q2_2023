6c6fef8683730d2eadc13cfecf6f2ecc1afd4934
tsua0002 <thomas.suau@ulb.be>
2023-05-03 23:16:35 +0200

    Achievements

--- launch.sh
+++ launch.sh
@@ -8,21 +8,33 @@ echo "############# Installation NGINX  ###################";
 sudo apt install nginx;
 
 # Copy html in /var/www/html with replacing previous index
+sudo rm -r /var/www/coursparticuliers;
 sudo mkdir /var/www/coursparticuliers;
-sudo cp ./html/* /var/www/coursparticuliers;
+sudo chown -R www-data:www-data /var/www/coursparticuliers;
+sudo cp -r ./html/* /var/www/coursparticuliers;
 
 # Replace nginx.conf file
 sudo rm /etc/nginx/nginx.conf;
 sudo cp ./nginx.conf /etc/nginx;
 
+# Add sites-available config
+sudo cp ./coursparticuliers.com /etc/nginx/sites-available
+sudo rm /etc/nginx/sites-enabled/default
+
+# Create symbolic link
+sudo ln -s /etc/nginx/sites-available/coursparticuliers.com /etc/nginx/sites-enabled/coursparticuliers.com
+
 # Add our domain name to hosts file
-sudo echo "0.0.0.0 CoursParticuliers.com www.CoursParticuliers.com" >> /etc/hosts;
+sudo bash -c  "echo '0.0.0.0 CoursParticuliers.com www.CoursParticuliers.com' >> /etc/hosts;"
 
 # Check that nginx config is ok
 echo "########## Check NGINX config  ##############";
 sudo nginx -t;
 
 # Need to have test before start nginx but I don't know how
+echo "############# Stop NGINX if is already running ###############";
+systemctl stop nginx;
+
 echo "############ START NGINX 	####################";
 systemctl start nginx;
 
