08f53f84236ef9ba4b62d3cf069ee50662dee600
tsua0002 <thomas.suau@ulb.be>
2023-05-03 21:31:24 +0200

    Website + refactoring script

--- launch.sh
+++ launch.sh
@@ -1,16 +1,17 @@
 #! /usr/bin/bash
+sudo apt-get update -y
 
 # Installation nginx
 echo "############# Installation NGINX  ###################";
 sudo apt install nginx;
 
 # Copy html in /var/www/html with replacing previous index
-rm -r /var/www/html;
-cp ./html/ /var/www;
+sudo rm -r /var/www/html;
+sudo cp ./html/ /var/www;
 
 # Replace nginx.conf file
 sudo rm /etc/nginx/nginx.conf;
-cp ./nginx.conf /etc/nginx;
+sudo cp ./nginx.conf /etc/nginx;
 
 # Check that nginx config is ok
 echo "########## Check NGINX config  ##############";
